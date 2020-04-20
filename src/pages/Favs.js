60cf100641ef690d863142432041991c14b056af
Kristian Lopez <kristian.lom@gmail.com>
2020-04-19 23:50:48 -0500

    Se agrega el uso de react-helmet

--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { FavsWithQuery } from '../container/GetFavorites'
+import { Layout } from '../components/Layout'
 
 export const Favs = () => {
   return (
-    <>
-      <h1>Favs</h1>
+    <Layout title='Tus Favoritos' subtitle='Aquí puedes encontrar tus favoritos'>
       <FavsWithQuery />
-    </>
+    </Layout>
   )
 }
