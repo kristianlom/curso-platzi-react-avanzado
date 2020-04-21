2f98342a8869a4dc6cf9cb3e6ebd163ba10f78b7
Kristian Lopez <kristian.lom@gmail.com>
2020-04-20 23:10:46 -0500

    Convirtiendo en PWA

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useContext, Suspense } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { Home } from './pages/Home'
 import { Redirect, Router } from '@reach/router'
 import { Detail } from './pages/Detail'
 import { NavBar } from './components/NavBar'
-import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
 
 import { Context } from './Context'
 import { NotFound } from './pages/NotFound'
 
+const Favs = React.lazy(() => import('./pages/Favs'))
+
 export const App = () => {
   const { isAuth } = useContext(Context)
   return (
-    <>
+    <Suspense fallback={<div />}>
       <GlobalStyle />
       <Logo />
       <Router>
@@ -31,6 +32,6 @@ export const App = () => {
         <User path='/user' />
       </Router>
       <NavBar />
-    </>
+    </Suspense>
   )
 }
--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { FavsWithQuery } from '../container/GetFavorites'
 import { Layout } from '../components/Layout'
 
-export const Favs = () => {
+export default () => {
   return (
     <Layout title='Tus Favoritos' subtitle='Aquí puedes encontrar tus favoritos'>
       <FavsWithQuery />
